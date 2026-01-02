d6190aaa4d8fee4179bd0b43387c8682a0a2b19d
medjbek <medjbek@gmail.com>
2026-01-02 14:46:55 +0100

    VG17 filtrage menus

--- app/Services/MenuService.php
+++ app/Services/MenuService.php
@@ -6,9 +6,31 @@ use App\Models\Menu;
 
 class MenuService
 {
-    public function getMenus(int $perPage = 10)
+    public function getMenus(array $filters = [], int $perPage = 10)
     {
         $query = Menu::with(['theme', 'diets', 'dishes.allergens', 'images']);
+
+        if (!empty($filters['theme_id'])) {
+            $query->where('theme_id', $filters['theme_id']);
+        }
+
+        if (!empty($filters['diet_id'])) {
+            $query->whereHas('diets', function ($q) use ($filters) {
+                $q->where('id', $filters['diet_id']);
+            });
+        }
+
+        if (!empty($filters['price_min'])) {
+            $query->where('base_price', '>=', $filters['price_min']);
+        }
+
+        if (!empty($filters['price_max'])) {
+            $query->where('base_price', '<=', $filters['price_max']);
+        }
+
+        if (!empty($filters['min_people'])) {
+            $query->where('minimum_people', '>=', $filters['min_people']);
+        }
 
         return $query->orderBy('title')
                      ->paginate($perPage);
